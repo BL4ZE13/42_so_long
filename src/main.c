22e41ba91fa6930057730a79e5cfeddda7eb0408
Diogo <diogo.santosss.2004@gmail.com>
2023-05-04 16:06:06 +0100

    images

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: diomarti <diomarti@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 13:03:20 by diomarti          #+#    #+#             */
-/*   Updated: 2023/05/02 15:21:37 by diomarti         ###   ########.fr       */
+/*   Updated: 2023/05/04 15:38:16 by diomarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,15 @@ void	init_img(t_all *img)
 {
 	int i;
 	
-	img->wall = mlx_xpm_file_to_image(img->mlx, "images/wall.xpm", &i, &i);
-	img->flor = mlx_xpm_file_to_image(img->mlx, "images/wall.xpm", &i, &i);
-	img->cl = mlx_xpm_file_to_image(img->mlx, "images/col.xpm", &i, &i);
-	img->en = mlx_xpm_file_to_image(img->mlx, "images/wall.xpm", &i, &i);
+	img->wall = mlx_xpm_file_to_image(img->mlx, "images/parede.xpm", &i, &i);
+	img->flor = mlx_xpm_file_to_image(img->mlx, "images/floor.xpm", &i, &i);
+	img->cl = mlx_xpm_file_to_image(img->mlx, "images/moeda.xpm", &i, &i);
+	img->en = mlx_xpm_file_to_image(img->mlx, "images/canto_dir.xpm", &i, &i);
 	img->pl_u = mlx_xpm_file_to_image(img->mlx, "images/pl_u.xpm", &i, &i);
 	img->pl_d = mlx_xpm_file_to_image(img->mlx, "images/pl_d.xpm", &i, &i);
 	img->pl_l = mlx_xpm_file_to_image(img->mlx, "images/pl_l.xpm", &i, &i);
 	img->pl_r = mlx_xpm_file_to_image(img->mlx, "images/pl_r.xpm", &i, &i);
-	img->ext_f = mlx_xpm_file_to_image(img->mlx, "images/porta_f.xpm", &i, &i);
+	img->ext_f = mlx_xpm_file_to_image(img->mlx, "images/porta.xpm", &i, &i);
 	// img->ext_a = mlx_xpm_file_to_image(img->mlx, "images/porta_a.xpm", &i, &i);
 	img->pl_i = img->pl_r;
 	img->nbr_cl = check_elements(img->map, 'C');
